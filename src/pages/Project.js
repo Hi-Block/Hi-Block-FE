a8f5f71c10b636d45f034e4fc90583cfcdfdb49a
qkrvudtn98 <ps3624@naver.com>
2022-08-18 01:47:46 +0900

    Project page 작업 (마크업)

---
+++ src/pages/Project.js
@@ -0,0 +1,25 @@
+// COMPONENTS
+import ProjectList from "../components/ProjectList";
+import SubNavigation from "../components/SubNavigation";
+import Pagination from "../components/Pagination";
+
+const Project = () => {
+    return (
+        <div className="project-area">
+            <div className="common-inner">    
+                <div className="title-area">
+                    <div className="title">Project</div>
+                    <div className="description">
+                        <span className="eng">HY-BLOCK</span>
+                        <span className="kr">의 프로젝트를 소개합니다!</span>
+                    </div>
+                </div>
+                <SubNavigation />
+                <ProjectList />
+                <Pagination />
+            </div>
+        </div>
+    );
+};
+
+export default Project;
