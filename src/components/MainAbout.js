cf7e0ecef61fb01b49f5c3b2fea3d8ce42cc16a3
cantsleep <cant_sleep@naver.com>
2022-08-19 21:08:24 +0900

    Main page Update

---
+++ src/components/MainAbout.js
@@ -0,0 +1,21 @@
+const MainAbout = () => {
+  return (
+    <div className="main-about-area">
+      <div className="common-inner">
+        <div className="image-area">
+          <span>image</span>
+        </div>
+        <div className="text-area">
+          <span className="title">
+            About <b>HY-BLOCK</b>
+          </span>
+          <span className="description">
+            hihihihihihihi hihihihihihihi hihihihihihihi hihihihihihihi
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainAbout;
