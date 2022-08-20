9138ab4d4e39f94bd534f3ceddd133c0837df369
qkrvudtn98 <ps3624@naver.com>
2022-08-21 00:25:02 +0900

    Add Navigation Item is-active property

---
+++ src/utils/navItem.js
@@ -0,0 +1,29 @@
+// Header menu items data
+
+export const navItem = [
+    {
+        id: 1,
+        content: 'Main',
+        address: ''
+    },
+    {
+        id: 2,
+        content: 'Article',
+        address: 'article'
+    },
+    {
+        id: 3,
+        content: 'Project',
+        address: 'project'
+    },
+    {
+        id: 4,
+        content: 'Member',
+        address: 'member'
+    },
+    {
+        id: 5,
+        content: 'Recruit',
+        address: 'recruit'
+    },
+];
