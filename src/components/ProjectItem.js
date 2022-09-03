64bc90a870f8f284652e789840e0088156299e98
qkrvudtn98 <ps3624@naver.com>
2022-09-03 17:14:34 +0900

    Execute Project Item (Demo)

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,6 +1,28 @@
-const ProjectItem = () => {
+//COMPONENTS
+import ProjectContributor from "./ProjectContributor";
+
+const ProjectItem = ({ name, desc, contributor }) => {
     return (
-        <div className="project-item">Item</div>
+        <div className="project-item">
+            <img
+                className="project-image" 
+                src={process.env.PUBLIC_URL+'/images/Logo-480.png'}
+                alt="projectlogo"
+            />
+            <div className="project-expr">
+                <div className="project-name">{ name }</div>
+                <div className="project-desc">{ desc }</div>
+                <div className="project-contributor">
+                    {contributor.map((it) => (
+                        <ProjectContributor contributor={it}/>
+                    ))}
+                </div>
+                <button className="btn-more">
+                    <span>더 알아보기</span>
+                    <img src={process.env.PUBLIC_URL+'/images/Arrow-More.png'} alt="arrow-more" />
+                </button>
+            </div>
+        </div>
     );
 }
 
